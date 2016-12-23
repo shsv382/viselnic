dabf0486d1c1bd70d8e8cbb1f610ec2204d21a78
shsv382 <shsv382@gmail.com>
2016-12-23 14:49:11 +0400

    Рабочая игра для одного игрока

--- viselnic.rb
+++ viselnic.rb
@@ -7,46 +7,71 @@ class Game
 		@mistakes = []
 #		@player_name = current_player.name
 		@step = 0
+#		$dev = 1								# Development mode
+	end
+
+	def mistake_count
+		@word.length - 1
 	end
 
 	def step
 		puts "Let's start the game!"
-		puts step_output(@word, nil)
-		while @mistakes.length < 6 || won?
-			puts "Input letter, please!"
-			current_letter = gets.chomp.upcase
-		if current_letter.length == 1
-			if @word.include?(current_letter)
-				puts step_output(@word, current_letter)
-			else
-				puts "Такой буквы нет!"
-				@mistakes << current_letter
-				print "Ошибки:"
-				@mistakes.each { |letter| print letter }
-			end
-		else
+		output = output_init(@word)
+		
+		until @mistakes.length == @word.length - 1 || won?(output)
+			@step += 1
+			puts
+#			puts "Последний шанс!" if @mistakes.length == @word.length - 2
+			puts "Шаг #{@step.to_s}"
+#			puts @word if $dev == 1				# Development mode
+			puts output
+			print "Ошибки(#{@mistake_count}): "
+			print @mistakes.join(", ")
+			print "\n"
 			puts "Введите одну букву!"
+			current_letter = gets.chomp.upcase
+			if current_letter.length == 1
+				if @word.include?(current_letter)
+					output = step_output(@word, current_letter, output)
+					puts output
+				else
+					puts "Такой буквы нет!"
+					@mistakes << current_letter unless @mistakes.include?(current_letter)
+				end
+			else
+			puts "Введите только одну букву!"
+			end
 		end
+		puts "Вы выиграли!" if (@mistakes.length < 6)
+		puts "Игра закончена!"
 	end
 
-	def step_output(word, current_letter)
-#		output = ""
-		if @step == 0
-			word << @word[0,1]
-			for i in 1...@word.length - 1
-				word << "_"
+	def output_init(word)
+			out = ""
+			out << word[0,1]
+			for i in 1...word.length - 1
+				out << "_"
 			end
-			word << @word[-1,1]
-		elsif current_letter
-			@word.split("").each_with_index { |chr, i| word[i] = current_letter if chr == current_letter }
+			out << word[-1,1]
+			out
+	end
+
+	def step_output(word, current_letter, out)
+
+		if current_letter
+			word.split("").each_with_index { |chr, i| out[i] = current_letter if chr == current_letter }
 		else
 			puts "Input error!"
 		end
-		word.join
+		out #.join
 	end
 
 	def won?(output)
-		true unless output.include?("_") 
+		if output.include?("_")
+			return false
+		else
+			return true
+		end 
 	end
 
 end
@@ -63,4 +88,11 @@ class Player
 end
 
 
+puts "Добро пожаловать в игру 'Висельник'"
 
+
+new_game = Game.new
+
+puts "Вы можете ошибиться #{new_game.mistake_count} раз!"
+
+new_game.step
